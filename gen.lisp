48430afde66f5d05419f259794d2e86aff019fa4
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-03-05 19:47:28 +0100

    clean up code a bit

--- gen.lisp
+++ gen.lisp
@@ -173,43 +173,30 @@
 			 (key_up selenium.webdriver.common.keys.Keys.ENTER)
 			 (key_up selenium.webdriver.common.keys.Keys.SHIFT)
 			 (perform)))
-	  (def start_ssh (self)
+	  (def start_ssh (self password)
 	    ;; https://gist.github.com/creotiv/d091515703672ec0bf1a6271336806f0
-	    (setf cmd (string3 "
-#Generate root password
+	    (setf cmd (dot (string3 "
 import random, string
-password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
-
-#Download ngrok
+password = {}
 ! wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 ! unzip -qq -n ngrok-stable-linux-amd64.zip
-#Setup sshd
 ! apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
-#Set root password
 ! echo root:$password | chpasswd
 ! mkdir -p /var/run/sshd
 ! echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 ! echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
 ! echo 'LD_LIBRARY_PATH=/usr/lib64-nvidia' >> /root/.bashrc
 ! echo 'export LD_LIBRARY_PATH' >> /root/.bashrc
-
-#Run sshd
 get_ipython().system_raw('/usr/sbin/sshd -D &')
-
-#Ask token
 print('Copy authtoken from https://dashboard.ngrok.com/auth')
 import getpass
 authtoken = getpass.getpass()
-
-#Create tunnel
 get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
-#Print root password
-print('Root password: {}'.format(password))
-#Get public address
 ! curl -s http://localhost:4040/api/tunnels | python3 -c \
 'import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])'
-"))
-	    )
+")
+			   (format password)))
+	    (self.run cmd))
 	  (def __init__ (self)
 	    (SeleniumMixin.__init__ self)
 	    (self.open_colab)
--- source/run_00_start.py
+++ source/run_00_start.py
@@ -96,41 +96,28 @@ class Colaboratory(SeleniumMixin):
         entry=self._driver.switch_to_active_element()
         entry.send_keys(code)
         selenium.webdriver.common.action_chains.ActionChains(self._driver).key_down(selenium.webdriver.common.keys.Keys.SHIFT).key_down(selenium.webdriver.common.keys.Keys.ENTER).key_up(selenium.webdriver.common.keys.Keys.ENTER).key_up(selenium.webdriver.common.keys.Keys.SHIFT).perform()
-    def start_ssh(self):
+    def start_ssh(self, password):
         cmd="""
-#Generate root password
 import random, string
-password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
-
-#Download ngrok
+password = {}
 ! wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 ! unzip -qq -n ngrok-stable-linux-amd64.zip
-#Setup sshd
 ! apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
-#Set root password
 ! echo root:$password | chpasswd
 ! mkdir -p /var/run/sshd
 ! echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 ! echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
 ! echo 'LD_LIBRARY_PATH=/usr/lib64-nvidia' >> /root/.bashrc
 ! echo 'export LD_LIBRARY_PATH' >> /root/.bashrc
-
-#Run sshd
 get_ipython().system_raw('/usr/sbin/sshd -D &')
-
-#Ask token
 print('Copy authtoken from https://dashboard.ngrok.com/auth')
 import getpass
 authtoken = getpass.getpass()
-
-#Create tunnel
 get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
-#Print root password
-print('Root password: {}'.format(password))
-#Get public address
 ! curl -s http://localhost:4040/api/tunnels | python3 -c 
 'import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])'
-"""
+""".format(password)
+        self.run(cmd)
     def __init__(self):
         SeleniumMixin.__init__(self)
         self.open_colab()
